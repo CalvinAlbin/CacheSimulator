b4a8a16333c6f4bb9925e1ac3246f1f31984841b
Calvin <calvin.albin@gmail.com>
2023-04-21 12:43:17 -0500

    Working CMD Input

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -3,9 +3,15 @@
 
 using namespace std;
 
-int main(){
-    cout << "hello";
-    cout << endl;
+int main(int argc, char* argv[]){
     
+    ifstream fin;
+    fin.open(argv[1]);
+    int foo;
+    fin >> foo;
+    fin.close();
+
+    cout << foo;
+
     return 0;
 }
