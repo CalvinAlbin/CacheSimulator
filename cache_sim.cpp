2b7a17927a2dbb05e27933e6f56287f7f7c1d875
Calvin <calvin.albin@gmail.com>
2023-04-22 14:52:27 -0500

    Finished Product

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -8,29 +8,57 @@ int main(int argc, char* argv[]){
 //Declaring initial variables
     int numEntries = atoi(argv[1]);
     int associativity = atoi(argv[2]);
-    int blockSpace = numEntries % associativity;
+    int blockSpace = numEntries / associativity;
 
-    const int numE {numEntries};
 
-//Opening file
+//Opening Input File
     ifstream fin;
     fin.open(argv[3]);
 
 
-//Initializing Cache Vector and initializing each index to -1
+//Opening Output File
+    ofstream fout;
+    fout.open("cache_sim_output.txt");
+
+
+//Initializing Cache Vector and blockIndex vector
     vector<int> cache;
-    for (int j = 0; j < numEntries; j++)
+    for (int j = 0; j < numEntries; j++){
         cache.push_back(-1);
+    }
+    vector<int> blockIndex;
+    for (int j = 0; j < associativity; j++){
+        blockIndex.push_back(0);
+    }
 
 
 //Itterating through entries
     int entry;
+    int section, sectionStartIndex;
+    int counter;
+    bool found;
     while (fin >> entry){
-        int section = entry % associativity;
+        section = entry % associativity;
+        counter = section * blockSpace;
+        sectionStartIndex = counter;
+        found = false;
+        while (!found && counter < blockSpace * (section + 1)){
+            if (cache[counter] == entry){
+                found = true;
+                fout << entry << " : HIT" << endl;
+            }
+            counter++;
+        }
+        if (found == false){
+            cache[blockIndex[section] + sectionStartIndex] = entry;
+            blockIndex[section] = (blockIndex[section] + 1) % blockSpace;
+            fout << entry << " : MISS" << endl;
+        }
     }
 
 
-
+//Close files and end program
     fin.close();
+    fout.close();
     return 0;
 }
