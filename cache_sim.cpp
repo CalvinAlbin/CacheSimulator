8b732f266102648a2827a6e24c093234f8c13a39
Calvin <calvin.albin@gmail.com>
2023-04-22 12:26:53 -0500

    Taking Int Inputs

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -4,9 +4,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+
+    int numEntries = atoi(argv[1]);
+    int associativity = atoi(argv[2]);
+
+    cout << numEntries + 1 << endl;
+    cout << associativity + 1 << endl;
     
     ifstream fin;
-    fin.open(argv[1]);
+    fin.open(argv[3]);
     int foo;
     fin >> foo;
     fin.close();
