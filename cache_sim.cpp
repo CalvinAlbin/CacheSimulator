347c8efec9527a2c18b21a8845231e471c3f8b5b
Calvin <calvin.albin@gmail.com>
2023-04-22 14:13:48 -0500

    Cache vector set up

--- cache_sim.cpp
+++ cache_sim.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-
+//Declaring initial variables
     int numEntries = atoi(argv[1]);
     int associativity = atoi(argv[2]);
-    
+    int blockSpace = numEntries % associativity;
+
+    const int numE {numEntries};
+
+//Opening file
     ifstream fin;
     fin.open(argv[3]);
- /*   int foo;
-    fin >> foo;
 
-    int foo2;
-    fin >> foo2;
 
-    cout << foo << " " << foo2;
-*/
-    int i;
-    while (fin >> i){
-        cout << i << endl;
+//Initializing Cache Vector and initializing each index to -1
+    vector<int> cache;
+    for (int j = 0; j < numEntries; j++)
+        cache.push_back(-1);
+
+
+//Itterating through entries
+    int entry;
+    while (fin >> entry){
+        int section = entry % associativity;
     }
 
 
-    
+
     fin.close();
     return 0;
 }
